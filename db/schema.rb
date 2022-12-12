065afb70b9c2aba0a8f36eede0c0c9ccdf76fc02
Justin Lamb <justinlamb@me.com>
2022-12-12 10:35:42 -0800

    posts and user models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_09_203136) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_12_183449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
