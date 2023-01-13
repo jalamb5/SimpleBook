efac18a754f0a176b979bfbe37b78b787a36d2ea
Justin Lamb <justinlamb@me.com>
2023-01-13 11:12:35 -0800

    create and show posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,8 @@ class PostsController < ApplicationController
 
   # GET /posts/1 or /posts/1.json
   def show
+    @post = Post.find(params[:id])
+    @author = User.find(@post.user_id)
   end
 
   # GET /posts/new
@@ -21,7 +23,7 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     respond_to do |format|
       if @post.save
