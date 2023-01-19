60b6e9f1d42efad3f6b90248c8b601e3c819171c
Justin Lamb <justinlamb@me.com>
2023-01-19 10:03:47 -0800

    likes controller updates

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,22 @@
 class LikesController < ApplicationController
+  def new
+    @like = Like.new
+  end
+
   def create
+    @post = Post.find(params[:post_id])
+    @like = @post.likes.create(like_params)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @like = @post.likes.find_by(like_params)
+    @like.destroy
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:user_id, :post_id)
   end
 end
