9fe06c636aea2eb6edf0c1f3e825c0c8d9d3c8b4
Justin Lamb <justinlamb@me.com>
2023-03-15 14:16:12 -0700

    generate relationships model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_213834) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_15_211358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_213834) do
     t.datetime "updated_at", null: false
     t.text "content"
     t.integer "user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
