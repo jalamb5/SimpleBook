62d560d01de5634952fa8b20f58ed737bbcf1755
Justin Lamb <justinlamb@me.com>
2023-03-22 12:35:17 -0700

    attempt request implementation

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,19 @@
 class RelationshipsController < ApplicationController
 
   def create
-    @user = User.find(params[:followed_id])
-    current_user.follow(@user)
+    if params.include?(:requested_id)
+      @user = User.find(params[:requested_id])
+      current_user.follow_request(@user)
+    else
+      @user = User.find(params[:followed_id])
+      current_user.follow(@user)
+    end
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
     end
   end
+
 
   def destroy
     @user = Relationship.find(params[:id]).followed
